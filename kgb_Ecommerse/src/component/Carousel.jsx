69dadf691986e015ac9b9e0d1e6cd73ebf95d924
gr7800 <gt29015@gmail.com>
2024-10-09 12:11:29 +0530

    product gap fiproblem fixe

--- kgb_Ecommerse/src/component/Carousel.jsx
+++ kgb_Ecommerse/src/component/Carousel.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-const Carousel = () => {
+const Carousel = ({
+  images = [],
+  autoSlide = false,
+  autoSlideInterval = 10000,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  useEffect(() => {
+    if (!autoSlide) return;
+    const slideInterval = setInterval(nextSlide, autoSlideInterval);
+    return () => clearInterval(slideInterval);
+  }, [currentIndex, autoSlide, autoSlideInterval]);
+
+  if (images.length == 0) {
+    return "...Loading";
+  }
+
   return (
-    <div className='bg-light-pink-1 h-screen'>Carousel</div>
-  )
-}
+    <div className="relative w-full h-[70vh]">
+      <div className="absolute translate-x-0 inset-0 transition-transform ease-out delay-500  h-[70vh] w-full flex justify-center items-center overflow-hidden border-2 border-gray-300 shadow-lg shadow-black">
+        <img
+          src={images[currentIndex]?.urls.full}
+          alt="Carousel Image"
+          className={`w-full h-[70vh] object-cover rounded-lg`}
+        />
+      </div>
+      <div
+        className="absolute top-1/2 left-6 z-10 transform -translate-y-1/2 cursor-pointer text-white bg-gray-800 opacity-[60%] p-2 rounded-full flex justify-center"
+        onClick={prevSlide}
+      >
+        <FaArrowLeft size={20} />
+      </div>
 
-export default Carousel
+      <div
+        className="absolute top-1/2 right-6 z-10 transform -translate-y-1/2 cursor-pointer text-white bg-gray-800 opacity-[60%] p-2 rounded-full flex justify-center"
+        onClick={nextSlide}
+      >
+        <FaArrowRight size={20} />
+      </div>
+
+      <div className="absolute bottom-4 w-full flex justify-center space-x-2">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`cursor-pointer w-3 h-3 rounded-full ${
+              index === currentIndex ? "bg-gray-800" : "bg-gray-400"
+            }`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- kgb_Ecommerse/src/pages/Blog.jsx
+++ kgb_Ecommerse/src/pages/Blog.jsx
@@ -14,7 +14,7 @@ const Blog = () => {
 
   return (
     <section className='bg-light-pink-1 w-full py-8 px-4 flex justify-center items-center'>
-      <div className="flex flex-wrap w-full justify-center gap-16">
+      <div className="flex flex-wrap w-full justify-center gap-5">
         {blogs && blogs.length > 0 && blogs.map(blog => (
           <BlogCard key={blog.id} {...blog} />
         ))}
