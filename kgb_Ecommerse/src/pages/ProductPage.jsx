09f791c15c2f782a0f75aefd657b25744d822387
rozodkarbharat <rozodkarbharat@gmail.com>
2024-10-09 11:09:32 +0530

    created pagination component

--- kgb_Ecommerse/src/pages/ProductPage.jsx
+++ kgb_Ecommerse/src/pages/ProductPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Pagination from '../component/Pagination';
 
 const ProductPage = () => {
+    const [page, setPage] = useState(1);
+  function gotoPage(pageIndex) {
+    setPage(pageIndex);
+  }
   return (
-    <div>ProductPage</div>
+    <div>
+      <div className='flex justify-end'>
+      <Pagination pageCount ={10} gotoPage={gotoPage} page={page} /> 
+      </div>
+    </div>
   )
 }
+
 
 export default ProductPage
