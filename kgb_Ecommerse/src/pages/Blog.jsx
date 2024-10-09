eaa0691adb98a91a28971e6f9b7f56398fcc95d4
inno <khushijohri2001@gmail.com>
2024-10-09 11:16:15 +0530

    Fix: Added dynamic data in Blog Page

--- kgb_Ecommerse/src/pages/Blog.jsx
+++ kgb_Ecommerse/src/pages/Blog.jsx
@@ -12,9 +12,9 @@ const Blog = () => {
 
   return (
     <section className='bg-light-pink-1 w-full py-8 px-4 flex justify-center items-center'>
-      <div className="flex flex-wrap w-full justify-center gap-6">
+      <div className="flex flex-wrap w-full justify-center gap-16">
         {blogs && blogs.length > 0 && blogs.map(blog => (
-          <BlogCard key={blog.id} blog={blog} />
+          <BlogCard key={blog.id} {...blog} />
         ))}
       </div>
     </section>
