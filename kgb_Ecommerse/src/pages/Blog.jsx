1b9ed466c2c9061faa63edb163899dd9fe31e5de
inno <khushijohri2001@gmail.com>
2024-10-09 10:37:47 +0530

    Added: Blog page

--- kgb_Ecommerse/src/pages/Blog.jsx
+++ kgb_Ecommerse/src/pages/Blog.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import BlogCard from "../component/Cards/BlogCard";
 
 const Blog = () => {
+  const blogs = [1, 2, 3, 4, 5, 6, 7, 7, 7]
   return (
-    <div>Blog</div>
-  )
+    <section className='bg-light-pink-1 w-full py-8 px-4 flex justify-center items-center'>
+      <div className="flex flex-wrap w-full justify-center gap-6">
+        {blogs.map(blog => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Blog
+export default Blog;
